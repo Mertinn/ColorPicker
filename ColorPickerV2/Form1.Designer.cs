4d4d33dc8656c47d4e4e964776403016739d09f3
Mertinn <marcin.rajs@wp.pl>
2021-02-14 14:36:10 +0100

    Initial commit, finished application

---
+++ ColorPickerV2/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace ColorPickerV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.rgbCopyBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageGetter = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rgbLbl = new System.Windows.Forms.Label();
+            this.hexLbl = new System.Windows.Forms.Label();
+            this.hexCopyBtn = new System.Windows.Forms.Button();
+            this.historyTable = new System.Windows.Forms.DataGridView();
+            this.rgbValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hevValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.loadColor = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.historyTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rgbCopyBtn
+            // 
+            this.rgbCopyBtn.Location = new System.Drawing.Point(78, 200);
+            this.rgbCopyBtn.Name = "rgbCopyBtn";
+            this.rgbCopyBtn.Size = new System.Drawing.Size(99, 23);
+            this.rgbCopyBtn.TabIndex = 1;
+            this.rgbCopyBtn.Text = "Kopiuj";
+            this.rgbCopyBtn.UseVisualStyleBackColor = true;
+            this.rgbCopyBtn.Click += new System.EventHandler(this.rgbCopyBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Location = new System.Drawing.Point(37, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // imageGetter
+            // 
+            this.imageGetter.DoWork += new System.ComponentModel.DoWorkEventHandler(this.imageGetter_DoWork);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 446);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kliknij CTRL, aby uruchomić.";
+            // 
+            // rgbLbl
+            // 
+            this.rgbLbl.AutoSize = true;
+            this.rgbLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rgbLbl.Location = new System.Drawing.Point(17, 203);
+            this.rgbLbl.Name = "rgbLbl";
+            this.rgbLbl.Size = new System.Drawing.Size(58, 16);
+            this.rgbLbl.TabIndex = 4;
+            this.rgbLbl.Text = "aRGB  - ";
+            // 
+            // hexLbl
+            // 
+            this.hexLbl.AutoSize = true;
+            this.hexLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.hexLbl.Location = new System.Drawing.Point(15, 240);
+            this.hexLbl.Name = "hexLbl";
+            this.hexLbl.Size = new System.Drawing.Size(60, 16);
+            this.hexLbl.TabIndex = 5;
+            this.hexLbl.Text = "     Hex  - ";
+            // 
+            // hexCopyBtn
+            // 
+            this.hexCopyBtn.Location = new System.Drawing.Point(78, 237);
+            this.hexCopyBtn.Name = "hexCopyBtn";
+            this.hexCopyBtn.Size = new System.Drawing.Size(99, 23);
+            this.hexCopyBtn.TabIndex = 6;
+            this.hexCopyBtn.Text = "Kopiuj";
+            this.hexCopyBtn.UseVisualStyleBackColor = true;
+            this.hexCopyBtn.Click += new System.EventHandler(this.hexCopyBtn_Click);
+            // 
+            // historyTable
+            // 
+            this.historyTable.AllowUserToResizeColumns = false;
+            this.historyTable.AllowUserToResizeRows = false;
+            this.historyTable.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.historyTable.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.historyTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.historyTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.rgbValue,
+            this.hevValue,
+            this.loadColor});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.historyTable.DefaultCellStyle = dataGridViewCellStyle1;
+            this.historyTable.Location = new System.Drawing.Point(2, 279);
+            this.historyTable.Name = "historyTable";
+            this.historyTable.ReadOnly = true;
+            this.historyTable.RowHeadersVisible = false;
+            this.historyTable.Size = new System.Drawing.Size(276, 152);
+            this.historyTable.TabIndex = 0;
+            this.historyTable.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.historyTable_CellClick);
+            // 
+            // rgbValue
+            // 
+            this.rgbValue.HeaderText = "RGB";
+            this.rgbValue.Name = "rgbValue";
+            this.rgbValue.ReadOnly = true;
+            // 
+            // hevValue
+            // 
+            this.hevValue.HeaderText = "HEX";
+            this.hevValue.Name = "hevValue";
+            this.hevValue.ReadOnly = true;
+            this.hevValue.Width = 67;
+            // 
+            // loadColor
+            // 
+            this.loadColor.FillWeight = 80F;
+            this.loadColor.HeaderText = "Wczytaj kolor";
+            this.loadColor.Name = "loadColor";
+            this.loadColor.ReadOnly = true;
+            this.loadColor.Text = "Wczytaj";
+            this.loadColor.UseColumnTextForButtonValue = true;
+            this.loadColor.Width = 80;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 481);
+            this.Controls.Add(this.historyTable);
+            this.Controls.Add(this.hexCopyBtn);
+            this.Controls.Add(this.hexLbl);
+            this.Controls.Add(this.rgbLbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.rgbCopyBtn);
+            this.MaximumSize = new System.Drawing.Size(264, 520);
+            this.MinimumSize = new System.Drawing.Size(264, 520);
+            this.Name = "Form1";
+            this.Text = "Color Picker";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.historyTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button rgbCopyBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker imageGetter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label rgbLbl;
+        private System.Windows.Forms.Label hexLbl;
+        private System.Windows.Forms.Button hexCopyBtn;
+        private System.Windows.Forms.DataGridView historyTable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rgbValue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hevValue;
+        private System.Windows.Forms.DataGridViewButtonColumn loadColor;
+    }
+}
+
---
+++ ColorPickerV2/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ColorPickerV2
+{
+    public partial class Form1 : Form
+    {
+        private string path = "history.txt";
+        private int counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            imageGetter.RunWorkerAsync();
+            RefreshTable();
+            historyTable.AllowUserToAddRows = false;
+
+        }
+
+        private void Main(int mouseX, int mouseY)
+        {
+            Bitmap bm = new Bitmap(10, 10);
+            Graphics g = Graphics.FromImage(bm);
+
+            g.CopyFromScreen(mouseX - 5, mouseY - 5, 0, 0, bm.Size);
+
+            Bitmap resizedBitmap = ResizeBitmap(bm, 150, 150);
+            g = Graphics.FromImage(resizedBitmap);
+            Pen pen = new Pen(Brushes.White, 1f);
+
+            g.DrawLine(pen, 0, 75, 150, 75);
+            g.DrawLine(pen, 75, 0, 75, 150);
+
+            pictureBox1.Image = resizedBitmap;
+
+            Bitmap screenshot = MakeScreenshot();
+
+            Color pixelColor = GetColorFromImage(screenshot, mouseX, mouseY);
+
+            SetCopyButtonsColor(pixelColor);
+        }
+
+        private Bitmap ResizeBitmap(Bitmap bmp, int width, int height)
+        {
+            Bitmap result = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.SmoothingMode = SmoothingMode.None;
+                g.InterpolationMode = InterpolationMode.NearestNeighbor;          
+                g.DrawImage(bmp, 0, 0, width, height);
+            }
+            return result;
+        }
+
+        private int[] GetMousePosition()
+        {
+            int[] positions = { Control.MousePosition.X, Control.MousePosition.Y };
+            return positions;
+        }
+
+        private Bitmap MakeScreenshot()
+        {
+            Bitmap bm = new Bitmap(Screen.PrimaryScreen.Bounds.Size.Width, Screen.PrimaryScreen.Bounds.Size.Height);
+            Graphics g = Graphics.FromImage(bm);
+            g.CopyFromScreen(0, 0, 0, 0, Screen.PrimaryScreen.Bounds.Size);
+
+            return bm;
+        }
+
+        private Color GetColorFromImage(Bitmap image, int mouseX, int mouseY)
+        {
+            Graphics g = Graphics.FromImage(image);
+            g.CopyFromScreen(0, 0, 0, 0, Screen.PrimaryScreen.Bounds.Size);
+            Color pixelColor = image.GetPixel(mouseX, mouseY);
+
+            return pixelColor;
+        }
+
+        private void SetCopyButtonsColor(Color color)
+        {
+            rgbCopyBtn.BackColor = color;
+            hexCopyBtn.BackColor = color;
+        }
+
+        private void imageGetter_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                int[] positions = GetMousePosition();
+
+                if(Control.ModifierKeys == Keys.Control)
+                {
+                    Main(positions[0], positions[1]);
+                }
+            }
+        }
+
+        private string ReplaceColorChars(string colorString)
+        {
+            colorString = colorString.Replace("Color ", "").Replace("[", "").Replace("]", "").Replace("A=", "").Replace("R=", "").Replace("G=", "");
+            colorString = colorString.Replace("B=", "");
+            return colorString;
+        }
+
+        private void RefreshTable()
+        {
+            historyTable.Rows.Clear();
+
+            string[] lines = File.ReadAllLines(path);
+
+            foreach(string line in lines)
+            {
+                string[] rgbHex = line.Split(new string[] { "|||" }, StringSplitOptions.RemoveEmptyEntries);
+                historyTable.Rows.Add(rgbHex[0], rgbHex[1]);
+            }
+
+        }
+
+        private void rgbCopyBtn_Click(object sender, EventArgs e)
+        {
+            Color rgbColor = rgbCopyBtn.BackColor;
+
+            string rgbColorString = ReplaceColorChars(rgbColor.ToString());
+            string hexColor = ConvertToHex(rgbColorString);
+
+            SaveToFile(rgbColorString, hexColor);
+
+            Clipboard.SetText(rgbColorString);
+
+            RefreshTable();
+        }
+
+        private void hexCopyBtn_Click(object sender, EventArgs e)
+        {
+            Color rgbColor = rgbCopyBtn.BackColor;
+
+            string rgbColorString = ReplaceColorChars(rgbColor.ToString());
+            string hexColor = ConvertToHex(rgbColorString);
+
+            SaveToFile(rgbColorString, hexColor);
+
+            Clipboard.SetText(hexColor);
+
+            RefreshTable();
+        }
+
+        private string ConvertToHex(string rgbColorString)
+        {
+            string[] rgbColors = rgbColorString.Split(',');
+            rgbColors[0] = "";
+
+            List<string> hexColors = new List<string>();
+            foreach (string color in rgbColors)
+            {
+                if (color != "")
+                {
+                    int colorInt = Convert.ToInt32(color);
+                    string hex = Convert.ToString(colorInt, 16);
+                    hexColors.Add(hex);
+                }
+            }
+
+            string hexColor = "";
+            foreach (string color in hexColors)
+            {
+                hexColor = hexColor + color;
+            }
+
+            for(int i = 0; i < 5; i++)
+            {
+                if (hexColor.Length != 6)
+                {
+                    hexColor = "0" + hexColor;
+                }
+            }
+            return hexColor;
+        }
+
+        private void historyTable_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (historyTable.Columns[e.ColumnIndex].Name == "loadColor")
+            {
+                SetCopyButtonsColor(GetRGBFromTable(e.RowIndex));
+            }
+        }
+
+        private Color GetRGBFromTable(int index)
+        {
+            string colorString = historyTable.Rows[index].Cells[0].Value.ToString();
+            string[] rgb = colorString.Split(',');
+            
+
+            Color color = Color.FromArgb(Convert.ToInt32(rgb[0]), Convert.ToInt32(rgb[1]), Convert.ToInt32(rgb[2]),
+            Convert.ToInt32(rgb[3]));
+
+            return color;
+        }
+
+        private void SaveToFile(string rgbColor, string hexColor)
+        {
+            string[] lines = File.ReadAllLines(path);
+            List<string> linesList = new List<string>(lines);
+
+            if(lines.Length > 4)
+            {
+                linesList[counter] = rgbColor + "|||" + hexColor;
+            }
+            else
+            {
+                linesList.Add(rgbColor + "|||" + hexColor);
+            }
+
+            File.WriteAllText(path, string.Empty);
+            using(StreamWriter sw = new StreamWriter(path, true))
+            {
+                foreach (string line in linesList)
+                {
+                    sw.WriteLine(line);
+                }
+            }
+
+            counter++;
+            if(counter > 4)
+            {
+                counter = 0;
+            }
+        }
+    }
+}
